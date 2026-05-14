36e2d4c8c9a3a91d2ec8f66450b46164b2f152e6
itsLys <keyn.shieda@gmail.com>
2026-05-14 20:08:32 +0100

    ROUTERchange FORBIDDEN -> NOT_FOUND" :

--- src/router/Router.cpp
+++ src/router/Router.cpp
@@ -61,7 +61,7 @@ RouteResult Router::resolve(const ServerConfig& server, const HttpRequest& reque
     if (isDirectory(result, reqPath) && isIndexed(result))
         return result;
     else if (result.location && result.location->autoindex == false)
-        return errorPage(status::FORBIDDEN, server.error_pages);
+        return errorPage(status::NOT_FOUND, server.error_pages);
     std::cout << "[]: isDirectory pass\n";
     return result;
 }
